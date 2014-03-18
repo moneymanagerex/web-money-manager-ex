6af1e28351dca566eeaa4672d23905788133e158
gabriele-v <gabriele-v@7a2cfc08-e8a9-4a5a-b7d1-8dda957a4c9f>
2014-03-18 19:52:44 +0000

    Test commit
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex-webapp/code@22 7a2cfc08-e8a9-4a5a-b7d1-8dda957a4c9f

--- WebApp/functions.php
+++ WebApp/functions.php
@@ -503,7 +503,7 @@ class db_function
         
         
         
-    //Delete group transaction
+    //Delete group of transaction
     function transaction_delete_group ($TrDeleteArr)
         {
             $const_dbpath = costant::database_path();
