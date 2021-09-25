2fbe5360cd9eb3a358d67d6b23c0cd16a1fcf6b2
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-09-25 10:04:57 +0300

    ADD remove closing tag for better security; change quotes for better performance

--- WebApp/configuration_user_default.php
+++ WebApp/configuration_user_default.php
@@ -3,11 +3,10 @@
 ##    User Setting   ##
 #######################
 
-$disable_authentication = "False";
-$user_username = "admin";
-$user_password = "admin";
-$disable_payee = "False";
-$disable_category = "False";
-$defaultaccountname = "Account1";
-$desktop_guid = "{54B63230-BDB5-FD75-3F44-8411E121CB4F}";
-?>
+$disable_authentication = 'False';
+$user_username = 'admin';
+$user_password = 'admin';
+$disable_payee = 'False';
+$disable_category = 'False';
+$defaultaccountname = 'Account1';
+$desktop_guid = '{54B63230-BDB5-FD75-3F44-8411E121CB4F}';
