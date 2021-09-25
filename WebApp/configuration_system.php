7d771eaa9f8ec2df28eef58c3f3478ff20494e7d
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-09-25 12:08:50 +0300

    ADD app version to 1.1.0

--- WebApp/configuration_system.php
+++ WebApp/configuration_system.php
@@ -4,7 +4,7 @@
 ##         don't touch              ##
 ######################################
 $dbpath = 'MMEX_New_Transaction.db';
-$app_version = '1.0.4';
+$app_version = '1.1.0';
 $api_version = '1.0.1';
 $tr_default_status = '';
 $tr_default_type = 'Withdrawal';
