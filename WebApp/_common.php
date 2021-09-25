d4045d595bc12efecfaa1efbb46c10c36db4aab1
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-09-25 13:04:21 +0300

    ADD better check if user can be authenticated

--- WebApp/_common.php
+++ WebApp/_common.php
@@ -6,8 +6,15 @@ require_once 'functions.php';
 
 if ($b_restricted_auth)
 {
-    session_start();
-    security::redirect_if_not_loggedin();
+
+    $const_username = costant::login_username();
+    $const_password = costant::login_password();
+
+    if (isset($const_username) AND isset($const_password))
+    {
+        session_start();
+        security::redirect_if_not_loggedin();
+    }
 }
 
 if (!isset($b_debug))
