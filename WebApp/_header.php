b4ff89328acd4d701fe2d5a74c93bb9e0f5ebd43
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2021-09-28 12:17:02 +0300

    ADD header accepts page specific styles and scripts as array params

--- WebApp/_header.php
+++ WebApp/_header.php
@@ -1,8 +1,9 @@
 <?php
     $s_debug = '';
     if ($b_debug) { $s_debug = '?'.microtime(true); }
-?>
-<!DOCTYPE HTML>
+    if (!isset($a_head_css_add)) { $s_head_css_add = ''; } else { $s_head_css_add = '        ' . implode ("\n" . '        ', $a_head_css_add) . "\n"; }
+    if (!isset($a_head_js_add)) { $s_head_js_add = ''; } else { $s_head_js_add = '        ' . implode ("\n" . '        ', $a_head_js_add) . "\n"; }
+?><!DOCTYPE HTML>
 <html lang="en">
     <head>
         <meta charset="utf-8" />
@@ -20,9 +21,17 @@
         <link rel="stylesheet" type="text/css" href="res/bootstrap-theme-3.3.6.min.css<?php echo $s_debug; ?>" />
         <link rel="stylesheet" type="text/css" href="res/style_global-1.1.0.css<?php echo $s_debug; ?>" />
 
+        <!-- optional css -->
+<?php echo $s_head_css_add ?>
+        <!-- /optional css -->
+
         <script src="res/jquery-2.1.4.min.js" type="text/javascript"></script>
         <script src="res/bootstrap-3.3.6.min.js" type="text/javascript"></script>
         <script src="res/app/functions-1.1.0.js<?php echo $s_debug; ?>" type="text/javascript"></script>
+
+        <!-- optional js -->
+<?php echo $s_head_js_add ?>
+        <!-- /optional js -->
 
     </head>
 
