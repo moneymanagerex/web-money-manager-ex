a3e9049e2589f2e0ee40efbcfa2b896332dfe7d9
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2022-06-09 17:25:43 +0200

    ADD more html at empty page

--- WebApp/show.php
+++ WebApp/show.php
@@ -1,7 +1,7 @@
 <?php
 
 $b_restricted_auth  = true;
-$s_page_title       = 'Show Transaction';
+$s_page_title       = 'Show Transactions';
 $a_head_js_add[]      = '<script src="res/app/base-1.0.4.js" type="text/javascript"></script>';
 $a_head_js_add[]      = '<script src="res/app/show-1.1.0.js" type="text/javascript"></script>';
 include_once '_common.php';
@@ -96,82 +96,78 @@ function drawRecordRow(Array $a_transaction, String $s_date) : void
 }
 
 $recordmaxid = db_function::transaction_select_maxid();
-if ($recordmaxid > 0 )
-    {
-        $resultarray = db_function::transaction_select_all_order_by_date('DESC');
-        echo '<div class="container">';
-            echo '<h3 class="text_align_center">' . (count($resultarray) == 0 ? 'No' : 'Current'). ' pending transaction' . (count($resultarray) != 1 ? 's' : ''). '</h3>';
+if ($recordmaxid > 0 ) :
+
+    $resultarray = db_function::transaction_select_all_order_by_date('DESC');
+    echo '<div class="container">';
+        echo '<h3 class="text_align_center">' . (count($resultarray) == 0 ? 'No' : 'Current'). ' pending transaction' . (count($resultarray) != 1 ? 's' : ''). '</h3>';
+        echo '<br />';
+        echo '<div class="table-responsive">';
+
+        include_once '_btn_back.php';
+
+            echo '<form id="Show_Function" class="form-show-function" method="post" action="show_function.php">';
+
+            echo '<input class="btn-edit" type="hidden" id="btn_action" name="btn_action" value="" />';
+            /**
+             *  edit selected transaction
+             */
+            echo '<input class="do-edit" type="hidden" id="TrEdit" name="TrEdit[]" value="" />';
+
+            /**
+             *  delete button
+             */
+            echo '<button type="submit" id="TrDelete" name="btn_action" value="Delete" class="btn btn-lg btn-danger btn-block">Delete all selected</button>';
+
+            /**
+             *  new button
+             */
+            echo '<input type="button" class="btn btn-lg btn-success btn-block" id="btn_new" value="New transaction" onclick='."'top.location.href = ".'"new_transaction.php"'."' />";
             echo '<br />';
-            echo '<div class="table-responsive">';
 
-                echo '<a href="landing.php" class="btn btn-lg btn-success btn-block">Return to menu</a>';
-                echo '<br />';
-
-                echo '<form id="Show_Function" class="form-show-function" method="post" action="show_function.php">';
-
-                echo '<input class="btn-edit" type="hidden" id="btn_action" name="btn_action" value="" />';
-                /**
-                 *  edit selected transaction
-                 */
-                echo '<input class="do-edit" type="hidden" id="TrEdit" name="TrEdit[]" value="" />';
-
-                /**
-                 *  delete button
-                 */
-                echo '<button type="submit" id="TrDelete" name="btn_action" value="Delete" class="btn btn-lg btn-danger btn-block">Delete all selected</button>';
-
-                /**
-                 *  new button
-                 */
-                echo '<input type="button" class="btn btn-lg btn-success btn-block" id="btn_new" value="New transaction" onclick='."'top.location.href = ".'"new_transaction.php"'."' />";
-                echo '<br />';
-
-                echo '<table class="table table-hover table-condensed">';
-                #echo '<table class = "table table-hover table-condensed table-bordered">'; //TABLE BORDERED FOR DEBUG
-                    echo '<thead>';
-                        echo '<tr>';
-                            echo "<th class='text_align_center'><span class='glyphicon glyphicon-trash'></span> <span class='transaction-extra-columns'>Delete</span></th>";
-                            echo "<th class=''><span class='glyphicon glyphicon-info-sign'></span> <span class='transaction-extra-columns'>Type</span></th>";
-                            echo '<th class="text_align_right">Amount</th>';
-                            echo '<th class="text_align_center">Notes</th>';
-                            echo '<th>Account</th>';
-                            if (costant::disable_payee() == False)
-                                {echo "<th class='transaction-extra-columns'>Payee</th>";}
-                            if (costant::disable_category() == False)
-                                {echo '<th>Category</th>';}
-                        echo '</tr>';
-                    echo '</thead>';
-                    
-                    echo '<tbody>';
-                    $s_date = '';
-                    for ($i = 0; $i <= $recordmaxid; $i++)
+            echo '<table class="table table-hover table-condensed">';
+            #echo '<table class = "table table-hover table-condensed table-bordered">'; //TABLE BORDERED FOR DEBUG
+                echo '<thead>';
+                    echo '<tr>';
+                        echo "<th class='text_align_center'><span class='glyphicon glyphicon-trash'></span> <span class='transaction-extra-columns'>Delete</span></th>";
+                        echo "<th class=''><span class='glyphicon glyphicon-info-sign'></span> <span class='transaction-extra-columns'>Type</span></th>";
+                        echo '<th class="text_align_right">Amount</th>';
+                        echo '<th class="text_align_center">Notes</th>';
+                        echo '<th>Account</th>';
+                        if (costant::disable_payee() == False)
+                            {echo "<th class='transaction-extra-columns'>Payee</th>";}
+                        if (costant::disable_category() == False)
+                            {echo '<th>Category</th>';}
+                    echo '</tr>';
+                echo '</thead>';
+                
+                echo '<tbody>';
+                $s_date = '';
+                for ($i = 0; $i <= $recordmaxid; $i++)
+                {
+                    if (isset($resultarray[$i]['ID']))
                     {
-                        if (isset($resultarray[$i]['ID']))
-                        {
-                            drawRecordRow($resultarray[$i], $s_date);
-                            $s_date = $resultarray[$i]['Date'];
-                        }
+                        drawRecordRow($resultarray[$i], $s_date);
+                        $s_date = $resultarray[$i]['Date'];
                     }
-                    echo '</tbody>';
-                echo '</table>';
-                echo '</form>';
-            echo '</div>';
+                }
+                echo '</tbody>';
+            echo '</table>';
+            echo '</form>';
         echo '</div>';
-    }
-else
-    {
-        echo '<div class="container">';
-            echo '<br />';
-            echo '<br />';
-            echo '<h3 class="text_align_center">No pending transactions</h3>';
-            echo '<br />';
-            echo '<br />';
-			echo '<a href="new_transaction.php" class="btn btn-lg btn-success btn-block">Add new</a>';
-            echo '<br />';
-            echo '<a href="landing.php" class="btn btn-lg btn-success btn-block">Return to menu</a>';
-            echo '<br />';
-            echo '<br />';
-        echo '</div>'."\n";
-    }
+    echo '</div>';
+
+else: ?>
+
+    <div class="container">
+        <h3 class="text_align_center">No pending transactions</h3>
+        <br />
+        <br />
+        <a href="new_transaction.php" class="btn btn-lg btn-success btn-block">Add new</a>
+
+<?php
+include_once '_btn_back.php';
+
+endif;
 
 include_once '_footer.php';
