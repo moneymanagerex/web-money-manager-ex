1d4c37e09d4d55cda4fcd4cb112dda7c2f410f65
Maksim Dzmitryew̆ <maksimdzmitryew@gmail.com>
2022-06-10 11:09:27 +0200

    ADD use unified pages list to build the menu

--- WebApp/landing.php
+++ WebApp/landing.php
@@ -5,28 +5,27 @@ include_once '_common.php';
 $s_page_title           = '';
 $b_page_logo            = true;
 include_once '_header.php';
-?>
 
-<?php
-#require_once "functions.php";
-#session_start();
-#security::redirect_if_not_loggedin();
+$pages_list = various::getPagesList();
+
 ?>
 
         <div class="container text_align_center">
-            <a href="new_transaction.php" class="btn btn-lg btn-success btn-block">New transaction</a>
-            <br />
-            <a href="show.php" class="btn btn-lg btn-success btn-block">Show transactions</a>
-            <br />
-            <a href="settings.php" class="btn btn-lg btn-success btn-block">Edit settings</a>
-            <br />
-            <a href="guide.php" class="btn btn-lg btn-success btn-block">Guide</a>
-            <br />
-            <a href="about.php" class="btn btn-lg btn-success btn-block">About</a>
-            <br />
-            <a href="logout.php" class="btn btn-lg btn-success btn-block">Logout</a>
-            <br />
-        </div>
-
 <?php
+foreach ($pages_list AS $page_url => $page_name):
+?>
+            <div class="div_btn">
+                <a href="<?php echo $page_url ?>.php" class="btn btn-lg btn-success btn-block"><?php
+                echo $page_name
+                ?></a>
+            </div>
+<?php
+endforeach;
+
+include_once '_btn_logout.php';
+
+?>
+        </div>
+<?php
+
 include_once '_footer.php';
